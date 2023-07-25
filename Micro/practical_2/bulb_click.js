f06d65261e7f91d35718431ff04acbd870814d4c
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-07-25 09:28:12 +0530

    updated

---
+++ Micro/practical_2/bulb_click.js
@@ -0,0 +1,9 @@
+let express = require("express");
+let app = express();
+let port = 6432;
+
+app.use(express.static(__dirname + "/bulb_click"));
+
+app.listen(port, function () {
+  console.log(`\nServer is being started on ${port}`);
+});
