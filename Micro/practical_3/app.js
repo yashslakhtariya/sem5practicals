5ac9aebf330f142ecca4e0a001813fcefa4e59f4
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-07-26 13:32:48 +0530

    updated

--- Micro/practical_3/app.js
+++ Micro/practical_3/app.js
@@ -1,9 +1,18 @@
 let DT = require("./dateTimeModule");
+let http = require("http");
 
-function Init() {
-  let dt = DT();
-  console.log(`\nDate : ${dt.tarikh}`);
-  console.log(`Time : ${dt.samay}`);
-}
-
-Init();
+http
+  .createServer(function (req, res) {
+    if (req.url === "/") {
+      res.write("\n\n\tWelcome to the YSL Date Time Module!\n\t");
+    }
+    if (req.url === "/datetime") {
+      let dt = DT();
+      res.write(`\n\n\tDate : ${dt.tarikh}`);
+      res.write(`\n\tTime : ${dt.samay}`);
+    }
+    res.end();
+  })
+  .listen(6432, function () {
+    console.log("\n\tServer is running on port 6432");
+  });
