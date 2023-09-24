6abbda572876b0fd59b5f5b2aa66b8d6a7b89686
Yash Lakhtariya <yashslakhtariya21@gnu.ac.in>
2023-09-24 21:58:07 +0530

    updated

--- Micro/practical_8/find.js
+++ Micro/practical_8/find.js
@@ -3,7 +3,7 @@ var url = "mongodb://yash:haribol@127.0.0.1:27017/?authMechanism=DEFAULT";
 MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db2) {
   if (err) throw err;
   var dbo = db2.db("db2");
-  dbo.collection("mycollection").findOne({}, function (err, result) {
+  dbo.collection("mycollection").find({}).toArray(function (err, result) {
     if (err) throw err;
     // console.log(result);
     console.log(result);
